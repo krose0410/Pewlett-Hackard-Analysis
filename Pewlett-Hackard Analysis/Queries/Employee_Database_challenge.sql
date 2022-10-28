ce0e4cf6f6ec9bec0849c546dbfe9ab9ee405599
krose0410 <109715441+krose0410@users.noreply.github.com>
2022-10-27 19:05:10 -0700

    Update Employee_Database_challenge.sql

--- Pewlett-Hackard Analysis/Queries/Employee_Database_challenge.sql
+++ Pewlett-Hackard Analysis/Queries/Employee_Database_challenge.sql
@@ -29,14 +29,21 @@ FROM unique_titles
 GROUP BY titles
 ORDER BY COUNT(titles) DESC;
 
-
--- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (______) _____,
-______,
-______,
-______
-
-INTO nameyourtable
-FROM _______
-WHERE _______
-ORDER BY _____, _____ DESC;
+--Deliverable part 2
+SELECT DISTINCT ON(e.emp_no)e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+t.titles
+--DISTINCT ON(emp_no)
+INTO mentor_eligibility
+FROM employees AS e
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1962-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY emp_no;
